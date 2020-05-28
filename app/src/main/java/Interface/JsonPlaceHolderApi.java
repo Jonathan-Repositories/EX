ee089df5adb8b20548f0247461a980b27e42e336
Irving Gerzain <irving.java@gmail.com>
2020-05-28 17:38:19 -0500

    Cambiar peticion

--- app/src/main/java/Interface/JsonPlaceHolderApi.java
+++ app/src/main/java/Interface/JsonPlaceHolderApi.java
@@ -5,12 +5,15 @@ import com.google.gson.JsonArray;
 import java.util.List;
 
 import Modelo.Posts;
+import Modelo.ResponseBody;
+import Modelo.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface JsonPlaceHolderApi {
 
     //@GET("posts")
     @POST("initial_load")
-    Call<Posts> getPosts();
+    Call<ResponseBody> getPosts(@Body Posts posts);
 }
